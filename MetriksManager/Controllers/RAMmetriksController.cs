8c5fc79af51a4d1b0ab471297a900741570e9670
trexa81 <trex81@yandex.ru>
2022-09-03 12:23:14 +0300

    add Controllers end MetricsManagerTests

---
+++ MetriksManager/Controllers/RAMmetriksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetriksManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RAMmetriksController : ControllerBase
+    {
+    }
+}
