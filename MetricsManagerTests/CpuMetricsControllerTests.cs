6c0ab5717298573bf34f33576826e269f10d0da8
trexa81 <trex81@yandex.ru>
2022-09-04 23:33:18 +0300

    CPUmetricsControllerTests

--- MetricsManagerTests/CpuMetricsControllerTests.cs
+++ MetricsManagerTests/CpuMetricsControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MetriksManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace MetricsManagerTests
 {
-    internal class CpuMetricsControllerTests
+    public class CPUmetricsControllerTests
     {
+        private CPUmetriksController _cpuMetricsController;
+
+        public CPUmetricsControllerTests()
+        {
+            _cpuMetricsController = new CPUmetriksController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnOk()
+        {
+            int agentId = 1;
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            var result = _cpuMetricsController.GetMetricsFromAgent(agentId, fromTime,
+            toTime);
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsAll_ReturnOk()
+        {
+            TimeSpan fromTime = TimeSpan.FromSeconds(0);
+            TimeSpan toTime = TimeSpan.FromSeconds(100);
+
+            var result = _cpuMetricsController.GetMetricsFromAll(fromTime, toTime);
+
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
     }
 }
